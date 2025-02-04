9e0c3606c5062aa9b5aeec23b8568998f2e85d7f
George <GoshaMareev@users.noreply.github.com>
2025-02-05 02:15:15 +0300

    -sl1.py

---
+++ sl1.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title('This is a title')
+st.header('This is a header')
+st.subheader('This is a subheader')
+st.text('This is a text')
+st.markdown('This is a markdown')
+st.latex(r'\int_a^b f(x) dx')
+st.write('This is a write')
